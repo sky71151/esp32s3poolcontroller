2719e769d22d43caaa697ef30dff228630ce02c4
Rob <vbtechnieken@gmail>
2025-08-29 09:07:14 +0200

    Nieuwe release v1.0.0

--- include/ota.h
+++ include/ota.h
@@ -3,4 +3,4 @@
 #include <HTTPUpdate.h>
 #include <WiFiClientSecure.h>
 
-const char* ota_url = "https://github.com/<user>/<repo>/releases/download/<tag>/firmware.bin";
+const char* ota_url = "https://github.com/sky71151/esp32s3poolcontroller/blob/main/.pio/build/esp32-s3-devkitc-1/bootloader.bin";
