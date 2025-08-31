b54f7b83f6309c158cbfe1125e20f1cc001b4361
Rob <vbtechnieken@gmail>
2025-08-31 19:12:47 +0200

    v28.6 Auto-commit: build

--- src/firebase.cpp
+++ src/firebase.cpp
@@ -16,11 +16,11 @@ void initFirebaseTask(void *pvParameters)
             // Firebase.reset(&config);
             config.api_key = API_KEY;
             config.database_url = DATABASE_URL;
-             auth.user.email = USER_EMAIL;
+            auth.user.email = USER_EMAIL;
             auth.user.password = USER_PASSWORD;
-            Firebase.begin(&config, &auth); // auth leeg laat anonieme login toe
-            Firebase.reconnectWiFi(true);
-            /*if (Firebase.signUp(&config, &auth, "", ""))
+            //Firebase.begin(&config, &auth); // auth leeg laat anonieme login toe
+            
+            if (Firebase.signUp(&config, &auth, "", ""))
             {
                 safePrintln("[FIREBASE] Anoniem ingelogd!");
             }
@@ -28,8 +28,8 @@ void initFirebaseTask(void *pvParameters)
             {
                 safePrint("[FIREBASE] Fout bij anoniem inloggen: ");
                 safePrintln(config.signer.signupError.message.c_str());
-            }*/
-
+            }
+            Firebase.reconnectWiFi(true);
             safePrintln("Firebase opnieuw geïnitialiseerd (anoniem)");
             firebaseInitialized = false; // reset status bij herinitialisatie
             streamConnected = false;
